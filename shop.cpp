ea82f4187ba1414bda0ca6368af42ce657c1be40
Abraham R <71684482+iDev90G@users.noreply.github.com>
2022-02-02 00:39:43 -0500

    update shop.cpp

--- shop.cpp
+++ shop.cpp
@@ -1,1 +1,35 @@
+/*system("cls");*/
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <string>
+#include <iomanip>
+#include <stdlib.h>
 
+using namespace std;
+
+int menu(void){
+  
+  cout<<"\n****************************Tienda Virtual****************************";
+  cout<<"\n1. Crear Cliente Nuevo";
+  cout<<"\n2. Ver Clientes Registrados";
+  cout<<"\n3. Crear Inventario";
+  cout<<"\n4. Ver Inventario";
+  cout<<"\n5. Vender un Producto;
+  cout<<"\n6. Salir";
+  cout<<"Ingrese su opción: ";
+}
+
+int main(){
+  int opcion, opc2, opc3, i = 0, a = 0, max= 5, cantidad_producto_vender, pregunta_descuento, descuento;
+  string nombre_producto_vender;
+  float precio_producto_vender, total_final, total, descuento_final, sub-total;
+  string nombre_cliente[50], nombre_producto[50];
+  float id[50], tel[50], precio_producto[50], codigo[50];
+  
+  do {
+    /*menu*/
+    /*switch*/
+  }
+}
