2986b1290c961893510b6a383c4788b68cfc9fe5
Abraham R <71684482+iDev90G@users.noreply.github.com>
2022-02-05 19:49:17 -0500

    update shop.cpp

--- shop.cpp
+++ shop.cpp
@@ -58,7 +58,7 @@ int main(){
        a++;
        cout<<"\nIngrese el Nombre del Producto:";
        cin>>nombre_producto[a];
-       cout<<"\nIngrese el Código del Producto;
+       cout<<"\nIngrese el Código del Producto";
        cin>>codigo[a];
        cout<<"\nIngrese el Precio del Producto: ";
        cin>>precio_producto[a];
