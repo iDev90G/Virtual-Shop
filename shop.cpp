a5481d1b0c55f018dd5c0b01058b4ad718dbfaa2
Abraham R <71684482+iDev90G@users.noreply.github.com>
2022-02-22 14:50:50 -0500

    update shop.cpp

--- shop.cpp
+++ shop.cpp
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-int menu(void){
-  
+int menu(void) {
+
   cout<<"\n****************************Tienda Virtual****************************";
   cout<<"\n1. Crear Cliente Nuevo";
   cout<<"\n2. Ver Clientes Registrados";
@@ -21,47 +21,66 @@ int menu(void){
   cout<<"Ingrese su opción: ";
 }
 
-int main(){
-  int opcion, opc2, opc3, i = 0, a = 0, max= 5, cantidad_producto_vender, pregunta_descuento, descuento;
+int main() {
+  int opcion,
+  opc2,
+  opc3,
+  i = 0,
+  a = 0,
+  max = 5,
+  cantidad_producto_vender,
+  pregunta_descuento,
+  descuento;
   string nombre_producto_vender;
-  float precio_producto_vender, total_final, total, descuento_final, sub-total;
-  string nombre_cliente[50], nombre_producto[50];
-  float id[50], tel[50], precio_producto[50], codigo[50];
-  
+  float precio_producto_vender,
+  total_final,
+  total,
+  descuento_final,
+  sub-total;
+  string nombre_cliente[50],
+  nombre_producto[50];
+  float id[50],
+  tel[50],
+  precio_producto[50],
+  codigo[50];
+
   do {
     menu();
     cin>>opcion;
-    
+
     switch (opcion) {
       case 1:
-       i++;
-       cout<<"\nIngrese el Nombre del nuevo Cliente: ";
-       cin>>nombre_cliente[i];
-       cout<<"\nIngrese el ID del nuevo Cliente\n*Sin espacios ni guiones: ";
-       cin>>id[i];
-       cout<<"\nIngrese el numero telefónico del nuevo Cliente\n*Sin espacios ni guiones: "
-       cin>>tel[i];
-       cout<<"\nIngrese 1 para volver al menú o 0 para salir: ";
-       cin>>opc2;
-       break;
-       
-       case 2:
-       for (i = 1, i < max,i++) {
-         cout<<"\nNombre del Cliente: "<<nombre_cliente[i]<<endl;
-         cout<<setprecision(0)<<fixed<<"\nNumero de ID del Cliente: "<<id[i]<<endl;
-       }
-       cout<<"\nIngrese 1 para volver al menú y 0 para salir: ";
-       cin>>opc2;
-       break;
-       
-       case 3:
-       a++;
-       cout<<"\nIngrese el Nombre del Producto: ";
-       cin>>nombre_producto[a];
-       cout<<"\nIngrese el Código del Producto: ";
-       cin>>codigo[a];
-       cout<<"\nIngrese el Precio del Producto: ";
-       cin>>precio_producto[a];
+      i++;
+      cout<<"\nIngrese el Nombre del nuevo Cliente: ";
+      cin>>nombre_cliente[i];
+      cout<<"\nIngrese el ID del nuevo Cliente\n*Sin espacios ni guiones: ";
+      cin>>id[i];
+      cout<<"\nIngrese el numero telefónico del nuevo Cliente\n*Sin espacios ni guiones: "
+      cin>>tel[i];
+      cout<<"\nIngrese 1 para volver al menú y 0 para salir: ";
+      cin>>opc2;
+      break;
+
+      case 2:
+      for (i = 1, i < max, i++) {
+        cout<<"\nNombre del Cliente: "<<nombre_cliente[i]<<endl;
+        cout<<setprecision(0)<<fixed<<"\nNumero de ID del Cliente: "<<id[i]<<endl;
+      }
+      cout<<"\nIngrese 1 para volver al menú y 0 para salir: ";
+      cin>>opc2;
+      break;
+
+      case 3:
+      a++;
+      cout<<"\nIngrese el Nombre del Producto: ";
+      cin>>nombre_producto[a];
+      cout<<"\nIngrese el Código del Producto: ";
+      cin>>codigo[a];
+      cout<<"\nIngrese el Precio del Producto: ";
+      cin>>precio_producto[a];
+      cout>>"\nIngrese 1 para volver al menú y 0 para salir: ";
+      cin>>opc2;
+      break;
     }
   }
 }
